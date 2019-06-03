9a0a6ec7378526dfc33037336376f83551131564
Asa Tirsen <asa.tirsen@gmail.com>
2019-06-03 04:01:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function nowServing(katzDeliLine) {
     if (katzDeliLine.length > 0) {
       return `The line is currently:${[katzDeliLine.indexOf() + ". "]}`;
     } else {
-      "The line is currently empty."
+      return "The line is currently empty.";
   }
 }
