deb51f0749f4c894f1c63de943ceda81d800ad2e
Asa Tirsen <asa.tirsen@gmail.com>
2019-06-03 04:19:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function nowServing(katzDeliLine) {
   }
 
   function currentLine(katzDeliLine) {
-    var key = katzDeliLine.indexOf(name);
     if (katzDeliLine.length > 0) {
-      return `The line is currently:${key} + ${[katzDeliLine]}}`;
+      for (i = 0; i < katzDeliLine.length; i++)
+      return `The line is currently:${i+1} + ${katzDeliLine}`;
     } else {
       return "The line is currently empty.";
   }
